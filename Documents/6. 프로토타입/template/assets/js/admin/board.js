bf054d568f258776b161c03fb4a63921451d45e3
JeonWooSung <neblasta85@gmail.com>
2024-03-27 00:43:27 +0900

    프로토타이핑
    -zTree 라이브러리 게시판 추가팝업에 적용중

--- Documents/6. 프로토타입/template/assets/js/admin/board.js
+++ Documents/6. 프로토타입/template/assets/js/admin/board.js
@@ -23,7 +23,7 @@
       }
     }
 
-    console.log("111111");
+    
     /**
      * Initiate Datatables
      */
@@ -56,9 +56,6 @@
     })
 
 
-    console.log("22222");
-
-
     /**
      * Initiate Modals
      */
@@ -75,31 +72,127 @@
          });   
      }
 
+  })();
 
-    // 트리뷰 참고
-    // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_treeview
-    // https://opentutorials.org/module/2398/13715
-    
-    // 노드
-    //  - 체크박스 (이미지/글자) (+)
-    // 리프
-    //  - (이미지/글자) (-)
-    const toggler = document.getElementsByClassName("caret");
-    for (let i = 0; i < toggler.length; i++)  
+
+/********************************
+ *  zTree 설정
+ *******************************/
+// zTree configuration information, refer to API documentation (setting details)
+var setting = {
+  view: {
+      addHoverDom: addHoverDom,
+      removeHoverDom: removeHoverDom,
+      selectedMulti: false,
+      showLine:false
+  },
+  check: {
+      enable: false,
+      chkStyle: 'radio',
+      radioType: "level"
+  },
+  data: {
+      simpleData: {
+          enable: true
+      }
+  },
+  edit: {
+      enable: true,
+      showRemoveBtn: true,
+      showRenameBtn:false,
+
+  }
+};
+const categoryOpenIconPath = "../assets/icon/layers.svg";
+const categoryCloseIconPath ="../assets/icon/layers-fill.svg";
+const attributeIconPath ="../assets/icon/puzzle.svg";
+
+// zTree data attributes, refer to the API documentation (treeNode data details)
+var zNodes =[
+
+  { id:1, pId:0, name:"카테고리1", isParent:true, open:true, iconOpen:categoryOpenIconPath, iconClose:categoryCloseIconPath},
+  { id:11, pId:1, name:"속성1" , icon:attributeIconPath},
+  { id:12, pId:1, name:"속성2" , icon:attributeIconPath},
+  { id:13, pId:1, name:"속성3" , icon:attributeIconPath},
+  { id:14, pId:1, name:"속성4" , icon:attributeIconPath},
+  { id:2, pId:0, name:"카테고리2", isParent:true, open:true, iconOpen:categoryOpenIconPath, iconClose:categoryCloseIconPath},
+  { id:21, pId:2, name:"속성1" , icon:attributeIconPath},
+  { id:22, pId:2, name:"속성1" , icon:attributeIconPath},
+  { id:23, pId:2, name:"속성1" , icon:attributeIconPath},
+  { id:24, pId:2, name:"속성1" , icon:attributeIconPath},
+  { id:3, pId:0, name:"카테고리3", isParent:true, open:true, iconOpen:categoryOpenIconPath, iconClose:categoryCloseIconPath},
+  { id:4, pId:0, name:"카테고리4", isParent:true, open:true, iconOpen:categoryOpenIconPath, iconClose:categoryCloseIconPath},
+  
+
+];
+var newCount = 1;
+function addHoverDom(treeId, treeNode) {
+  const sObj = $("#" + treeNode.tId + "_span");
+  if (treeNode.editNameFlag || $("#addBtn_"+treeNode.tId).length>0) return;
+
+  // 부모 객체인경우 + 버튼 추가.
+  if(treeNode.isParent == true){
+    const addStr = "<span class='button add' id='addBtn_" + treeNode.tId
+              + "' title='add node' onfocus='this.blur();'></span>";
+
+    sObj.after(addStr);
+
+    var btn = $("#addBtn_"+treeNode.tId);
+    if (btn) btn.bind("click", function()
     {
-      toggler[i].addEventListener("click", function() 
-      {
-        console.log("111", this.parentElement);
-        console.log("222", this.parentElement.parentElement);
-        console.log("333", this.parentElement.parentElement.querySelector(".nested"));
-        console.log("444", this.parentElement.parentElement.querySelector(".nested").classList);
-        console.log("555", this.parentElement.parentElement.querySelector(".nested").classList.toggle("active"));
-        // 여기 토글하면 안바뀜. 모달이라서 그런거 같으니 remove / add로 변경할 것 to do
-        
-        this.parentElement.parentElement.querySelector(".nested").classList.toggle("active");
-        this.classList.toggle("caret-down");
-      });
+      // 여기서 속성 추가 팝업을 호출 할 것.
+        //var zTree = $.fn.zTree.getZTreeObj("tree_reader");
+        //zTree.addNodes(treeNode, {id:(100 + newCount), pId:treeNode.id, name:"new node" + (newCount++)});
+      console.log("111",treeNode.id );
+        addAttribute("tree_reader",  treeNode.id, "Attribute" + (attributeIdCount));
+        return false;
+    });
+
+  }
+};
+function removeHoverDom(treeId, treeNode) {
+  $("#addBtn_"+treeNode.tId).unbind().remove();
+};
+
+
+// 이거 호출 작업 해야함.
+let categoryIdCount = 1;
+function addCategory(treeName, categoryName){
+  const zTree = $.fn.zTree.getZTreeObj(treeName);
+  if(zTree != null){
+      zTree.addNodes(
+        null, 
+        { id:(100 + (categoryIdCount++)), pId:0, name:categoryName, isParent:true, open:true, iconOpen:categoryOpenIconPath, iconClose:categoryCloseIconPath}
+      );
+  }
+}
+
+let attributeIdCount = 1;
+function addAttribute(treeName, parentTreeId, attributeName){
+  const zTree = $.fn.zTree.getZTreeObj(treeName);
+  if(zTree != null){
+    let parentNode = null;
+    
+    let nodes = zTree.getNodes();
+    for(let i=0;i<nodes.length;i++){
+      if(nodes[i].id == parentTreeId){
+        treeNode = nodes[i];
+        break;
+      }
     }
 
+    if(parentNode == null){
+      console.log("addAttribute fail. parentNode is null");
+      return;
+    }
 
-  })();
+    zTree.addNodes( parentNode, { id:(1000 + (attributeIdCount++)), pId:parentTreeId, name:attributeName, icon:attributeIconPath});
+  }
+}
+
+$(document).ready(function(){
+  //zTreeObj = $.fn.zTree.init($("#treeDemo"), setting, zNodes);
+  $.fn.zTree.init($("#tree_reader"), setting, zNodes);
+
+  //$.fn.zTree.init($("#tree_writer"), setting, zNodes);
+});
