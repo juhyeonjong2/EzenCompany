00136f0b48c7835bd416136f3dcdbb6e8f25fe56
JeonWooSung <neblasta85@gmail.com>
2024-03-28 16:29:52 +0900

    프로토타이핑
    - 블로그 작업중

---
+++ Documents/6. 프로토타입/EzenCompany/assets/js/blog/blog.js
@@ -0,0 +1,42 @@
+
+
+//var zTreeObj;
+
+// zTree configuration information, refer to API documentation (setting details)
+var setting = {
+    view: {
+        selectedMulti: false,
+        showLine:false
+    },
+    check: {
+        enable: false,
+        chkStyle: 'radio',
+        radioType: "level"
+    },
+    data: {
+        simpleData: {
+            enable: true
+        }
+    },
+    edit: {
+        enable: false,
+    }
+};
+// zTree data attributes, refer to the API documentation (treeNode data details)
+var zNodes =[
+
+    { id:1, pId:0, name:"스프링 일지", open:true},
+        { id:11, pId:1, name:"스프링 일지 1" },
+        { id:12, pId:1, name:"스프링 일지 2" },
+    { id:2, pId:0, name:"임시", open:true},
+        { id:21, pId:2, name:"임시 폴더" ,open:true},
+            { id:221, pId:21, name:"[임시]임시로만든 게시물" },
+    
+    
+
+];
+
+$(document).ready(function(){
+    $.fn.zTree.init($("#blog_tree"), setting, zNodes);
+});
+
