11fc27fc3518dd09d11d8e95b3456dedc99fb688
juhyeonjong <vkdlfjfxm2@naver.com>
2024-04-05 16:48:35 +0900

    게시판 목록 페이지 구현 중

--- Project/src/main/java/ezen/ezencompany/controller/BoardController1.java
+++ Project/src/main/java/ezen/ezencompany/controller/BoardController1.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ezen.ezencompany.vo.UserVO;
 
 
 @RequestMapping(value="/board")
@@ -19,6 +23,13 @@ public class BoardController1 {
 	@RequestMapping(value="/home")
 	public String main(Model model) {
 		//Map 형식으로 분류들을 전부 가져온다 -> 키와 벨류가 없는 것도 가져와야해서 리스트로 변경
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		UserVO user = (UserVO) authentication.getPrincipal();
+		
+		System.out.println(user.getMno()+"팔번이 나와야해");
+		
+		
 		List<Object> cate 
 			=  sqlSession.selectList("ezen.ezencompany.mapper.categoryMapper.selectCategory", null);
 		model.addAttribute("cate", cate);
